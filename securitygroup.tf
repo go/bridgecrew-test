4ae21295a2f34eb6d2410023bef16ecc815e7020
Go Chiba <go.chiba@gmail.com>
2021-06-02 10:30:34 +0900

    initial commit

---
+++ securitygroup.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "demo" {
+  name        = "demo"
+  description = "Security group for demo node"
+  vpc_id      = "${aws_vpc.demo.id}"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = "${
+    tomap({
+     "Name" = "sg-demo",
+    })
+  }"
+}
+
+resource "aws_security_group_rule" "demo-ingress-ssh" {
+  description              = "SSH Access for worker nodes"
+  from_port                = 22
+  protocol                 = "tcp"
+  security_group_id        = "${aws_security_group.demo.id}"
+  to_port                  = 22
+  type                     = "ingress"
+  cidr_blocks              = ["${local.workstation-external-cidr}"]
+}
+
+resource "aws_security_group_rule" "demo-ingress-http" {
+  description              = "HTTP Access for worker nodes"
+  from_port                = 80
+  protocol                 = "tcp"
+  security_group_id        = "${aws_security_group.demo.id}"
+  to_port                  = 80
+  type                     = "ingress"
+  cidr_blocks              = ["0.0.0.0/0"]
+}
---
+++ variables.tf
@@ -0,0 +1,22 @@
+#
+# Variables Configuration
+#
+
+variable "vpc_cidr_block" {
+  default = "192.168.10.0/24"
+  type    = string
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+  type    = string
+}
+
+variable "key_name" {
+  default = "g-chiba-key"
+  type    = string
+}
+
+variable "instance_name" {
+  type    = string
+}
