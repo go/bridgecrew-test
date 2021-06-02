ef22a781b2548d14a242abc6a8330c300e2ce0d7
Go Chiba <go.chiba@gmail.com>
2021-06-02 10:48:13 +0900

    Add unsafe security rule

--- securitygroup.tf
+++ securitygroup.tf
@@ -17,6 +17,16 @@ resource "aws_security_group" "demo" {
   }"
 }
 
+resource "aws_security_group_rule" "unsafe-ssh" {
+  description              = "SSH Access for all"
+  from_port                = 22
+  protocol                 = "tcp"
+  security_group_id        = "${aws_security_group.demo.id}"
+  to_port                  = 22
+  type                     = "ingress"
+  cidr_blocks              = ["0.0.0.0/0"]
+}
+
 resource "aws_security_group_rule" "demo-ingress-ssh" {
   description              = "SSH Access for worker nodes"
   from_port                = 22
