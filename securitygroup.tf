97c39da2388cb54fe7ab151d5431bb8926790f9f
Go Chiba <go.chiba@gmail.com>
2021-06-08 14:13:08 +0900

    Add insecure sg rule

--- securitygroup.tf
+++ securitygroup.tf
@@ -15,6 +15,16 @@ resource "aws_security_group" "demo" {
   }))
 }
 
+resource "aws_security_group_rule" "insecure-ssh" {
+  description       = "SSH Access for ALL"
+  from_port         = 22
+  protocol          = "tcp"
+  security_group_id = aws_security_group.demo.id
+  to_port           = 22
+  type              = "ingress"
+  cidr_blocks       = "0.0.0.0/0"
+}
+
 resource "aws_security_group_rule" "demo-ingress-ssh" {
   description       = "SSH Access for worker nodes"
   from_port         = 22
