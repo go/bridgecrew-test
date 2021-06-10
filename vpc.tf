69af8b728e43357b16a3938acfeb0f5380cfbde8
Go Chiba <go.chiba@gmail.com>
2021-06-10 11:10:06 +0900

    Update resorces name

--- vpc.tf
+++ vpc.tf
@@ -6,45 +6,45 @@
 #  * Route Table
 #
 
-resource "aws_vpc" "demo" {
+resource "aws_vpc" "bridgecrew-demo" {
   cidr_block           = var.vpc_cidr_block
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags = (tomap({
-    "Name" = "vpc-demo",
+    "Name" = "bridgecrew-demo-vpc",
   }))
 }
 
-resource "aws_subnet" "demo" {
+resource "aws_subnet" "bridgecrew-demo" {
   count = 3
 
   availability_zone       = data.aws_availability_zones.available.names[count.index]
-  cidr_block              = cidrsubnet(aws_vpc.demo.cidr_block, 2, count.index)
-  vpc_id                  = aws_vpc.demo.id
+  cidr_block              = cidrsubnet(aws_vpc.bridgecrew-demo.cidr_block, 2, count.index)
+  vpc_id                  = aws_vpc.bridgecrew-demo.id
   map_public_ip_on_launch = true
 
   tags = (tomap({
-    "Name" = "subnet-demo0${count.index}",
+    "Name" = "bridgecrew-demo-subnet0${count.index}",
   }))
 }
 
-resource "aws_internet_gateway" "demo" {
-  vpc_id = aws_vpc.demo.id
+resource "aws_internet_gateway" "bridgecrew-demo" {
+  vpc_id = aws_vpc.bridgecrew-demo.id
 }
 
-resource "aws_route_table" "demo" {
-  vpc_id = aws_vpc.demo.id
+resource "aws_route_table" "bridgecrew-demo" {
+  vpc_id = aws_vpc.bridgecrew-demo.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.demo.id
+    gateway_id = aws_internet_gateway.bridgecrew-demo.id
   }
 }
 
-resource "aws_route_table_association" "demo" {
+resource "aws_route_table_association" "bridgecrew-demo" {
   count = 3
 
-  subnet_id      = aws_subnet.demo.*.id[count.index]
-  route_table_id = aws_route_table.demo.id
+  subnet_id      = aws_subnet.bridgecrew-demo.*.id[count.index]
+  route_table_id = aws_route_table.bridgecrew-demo.id
 }
